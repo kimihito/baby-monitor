f979f1884579fcbad167062781d7cc7ac79e6a7a
kimihito <tatsurotamashiro@gmail.com>
2019-02-15 22:20:26 +0900

    :sparkles: Add buttons to live or watch

---
+++ components/HeaderNav.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand to="/">Baby monitor</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNav'
+}
+</script>
